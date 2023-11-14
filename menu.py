187e621029748c37f3d0085d3464bef3a124ea42
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-11-14 12:37:55 -0500

    Implemented multithreading for large directories also includes recursive bs for directories within directories which does not quite work yet

--- menu.py
+++ menu.py
@@ -1,29 +1,37 @@
-import platform
+
 import os
 from celery import Celery
 from src.autoRsync import autoRsync
+import threading
 
-id = platform.uname()
+
 
 
 source = input("Enter Source Directory: ")
 dest = input("Enter Destination Directory: ")
 
 
-if platform.system() == 'Windows':
-    autoRsync.windows(source, dest)
-elif platform.system() =='Darwin':
-    if id[5] == 'i386':
-        autoRsync.macos_x86(source, dest)
-    else:
-        autoRsync.macos_arm(source, dest)
-elif platform.system() == 'linux':
-    if id[5] == 'x86_64':
-        autoRsync.linux_x86(source, dest)
-    else:
-        autoRsync.linux_arm(source, dest)
-elif platform.system() == 'freebsd':
-    autoRsync.freebsd(source, dest)
+if not (os.path.isdir(dest)):
+    print(f"Destination directory {dest} does not exist, create now? (y/n)")
+    opt = input()
+    if(opt == 'y' or 'yes'):
+        os.makedirs(dest)
+if source.endswith("/"):
+    files = os.listdir(source)
+    mtf = 1 #flag for multithreading use
+
+
+if(mtf):
+    for file in files: #create a thread for each file in directory, call correct autoRsync member function and run in parallel
+        tempsource = source + file
+        if(os.path.isdir(tempsource)): # RECURSIVE BULLSHIT IS GREAT and DOESN'T WORK
+            tempfiles = os.listdir(tempsource)
+            for tempfile in tempfiles:
+                temptemp = tempsource + tempfile
+                tt = threading.Thread(target=autoRsync.find_os(temptemp, dest))
+        print(tempsource)
+        t = threading.Thread(target=autoRsync.find_os(tempsource, dest))
 
 else:
-    print("Unsupported Operating system", id[0])
+
+    autoRsync.find_os(source, dest)
--- src/autoRsync.py
+++ src/autoRsync.py
@@ -1,9 +1,37 @@
 import sysrsync
 import os
+import platform
 
 
 class autoRsync:
+
+
     '''
+    find_os will take in the source and destination directories or files, determine what OS both 
+    directories are hosted on, and make the handshake with the correct method for the correct OS
+    '''
+
+    def find_os(source, dest):
+        id = platform.uname()
+
+        if platform.system() == 'Windows':
+            autoRsync.windows(source, dest)
+        elif platform.system() =='Darwin':
+            if id[5] == 'i386':
+                autoRsync.macos_x86(source, dest)
+            else:
+                autoRsync.macos_arm(source, dest)
+        elif platform.system() == 'linux':
+            if id[5] == 'x86_64':
+                autoRsync.linux_x86(source, dest)
+            else:
+                autoRsync.linux_arm(source, dest)
+        elif platform.system() == 'freebsd':
+            autoRsync.freebsd(source, dest)
+
+        else:
+            print("Unsupported Operating system", id[0])
+        '''
     when windows is detected, autoRsync will call sysrsync and copy
     a selected folder to a selected destination
     '''
@@ -18,7 +46,7 @@ class autoRsync:
     a selected folder to a selected destination
     '''
     def macos_arm(source, dest):
-        print("Starting for macos_arm\n\n")
+        #print("Starting for macos_arm\n\n")
         sysrsync.run(source=source, destination=dest)
         fname = source.split("/")
         nName = dest + fname[len(fname)-1]
