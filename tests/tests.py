a2f54f094f5ef0cc2f0727f36bd1e312f53aa989
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-11-17 08:07:17 -0500

    wow, love this

--- tests/tests.py
+++ tests/tests.py
@@ -5,7 +5,7 @@ from ..src import autoRsync
 
 @pytest.fixture(scope="module")
 def platform_test():
-    autoRsync.find_os('/testA/', '/testB/')
+    autoRsync.autoRsync.find_os('/testA/', '/testB/')
 
 #Connect to 2 S3 Instances, using $S3_TOKEN and $S3_TOKEN2 and perform tests with their respective OS's
 #Transmit ~1 GB between the two and then back.
